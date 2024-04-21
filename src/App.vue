b102e9408af3e7321f23a1d835945f0d414d7c37
kevinslashgun <kevinbarragan1402@gmail.com>
2024-04-21 18:02:59 +0200

    Add ComputedProperties component to App.vue template

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
+import ComputedProperties from './components/ComputedProperties.vue'
 </script>
 
 <template>
   <h1>Hello</h1>
+  <ComputedProperties />
 </template>
 
 <style scoped></style>
---
+++ src/components/ComputedProperties.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref, computed } from 'vue'
+const number = ref(5)
+const squaredNumber = computed(() => number.value * number.value)
+
+const firstName = ref('John')
+const lastName = ref('Doe')
+
+const fullName = computed(() => `${firstName.value} ${lastName.value}`)
+
+const counter = ref(0)
+const squaredCounter = computed(() => counter.value ** 2)
+</script>
+
+<template>
+    <h1>Original Number: {{ number }}</h1>
+    <h1>Squared Number: {{ squaredNumber }}</h1>
+    <h1>Full Name: {{ fullName }}</h1>
+    <h1>Counter: {{ counter }}</h1>
+    <h1>Squared Counter: {{ squaredCounter }}</h1>
+    <button @click="counter++">Increment Counter</button>
+</template>
