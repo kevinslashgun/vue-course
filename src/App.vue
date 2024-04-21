bce02dc89bd6f6e4b3586ae54c7d535daae70f9e
kevinslashgun <kevinbarragan1402@gmail.com>
2024-04-21 18:48:53 +0200

    Add ConditionalRendering component to App.vue template

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
+import ConditionalRendering from './components/ConditionalRendering.vue';
 </script>
 
 <template>
   <h1>Hello</h1>
+  <ConditionalRendering />
 </template>
 
 <style scoped></style>
---
+++ src/components/ConditionalRendering.vue
@@ -0,0 +1,11 @@
+<script setup>
+const isUserLoggedIn = false;
+</script>
+
+<template>
+    <div>
+        <h2 v-if="isUserLoggedIn">Welcome, User!</h2>
+        <h2 v-else>Welcome, Guest!</h2>
+        <p v-show="isUserLoggedIn"> This paragraph is visible if you are logged.</p>
+    </div>
+</template>
