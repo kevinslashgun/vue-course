c80dbcd73cc50568f435cdd92f53f496ad47540c
kevinslashgun <kevinbarragan1402@gmail.com>
2024-04-21 18:56:35 +0200

    Add FormComponent and update App.vue template

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <script setup>
+import FormComponent from './components/FormComponent.vue';
+
+const formHandler = (name, email) => {
+  console.log(name, email);
+}
 </script>
 
 <template>
   <h1>Hello</h1>
+  <FormComponent @userInfo="formHandler" />
 </template>
 
 <style scoped></style>
---
+++ src/components/FormComponent.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue'
+const name = ref('')
+const email = ref('')
+</script>
+
+<template>
+    <form @submit.prevent="$emit('userInfo', name, email)">
+        <input type="text" placeholder="Name" v-model="name" />
+        <input type="email" placeholder="Email" v-model="email" />
+        <button>Submit</button>
+    </form>
+</template>
